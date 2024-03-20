3bd3ce98b5b1e9511cfcafb27467a2d04bf4a4f0
Evan <evan.eithan24@gmail.com>
2024-03-19 23:17:34 -0300

    Switch to english comments and variables

--- test.py
+++ test.py
@@ -9,45 +9,46 @@ import matplotlib.pyplot as plt
 
 # print(selenium.__version__)
 
+# Heres your Buscalibre Wishlist URL
 url = "https://www.buscalibre.cl/v2/cosmere_1647219_l.html"
 
-# Para que no se abra el navegador
+# Chrome UI settings for headless (Silent) mode
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
 
 try:
-  # Ir a la lista de Buscalibre pasado por variable
+  # Go to the URL
   driver.get(url)
 
-  # Esperar a que cargue la pagina
+  # Wait for the page to load
   WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
-  # Obtener la cantidad de libros
-  cantidad = driver.find_elements(By.CLASS_NAME, 'contenedorProducto').__len__()
-  # time.sleep(1)
+  # Get the amount of books
+  booksQuantity = driver.find_elements(By.CLASS_NAME, 'contenedorProducto').__len__()
 
-  for i in range(cantidad):
-    # Conseguimos los datos
-    titulo = driver.find_elements(By.CSS_SELECTOR, '.infoProducto .titulo')[i].text
-    precioAhora = int(driver.find_elements(By.CSS_SELECTOR, '.infoProducto .precioAhora')[i].text.replace("$ ", "").replace(".", ""))
-    #Comprobamos si el libro está en descuento
+  for i in range(booksQuantity):
+    # Get the book's title, price and discount
+    title = driver.find_elements(By.CSS_SELECTOR, '.infoProducto .titulo')[i].text
+    priceNow = int(driver.find_elements(By.CSS_SELECTOR, '.infoProducto .precioAhora')[i].text.replace("$ ", "").replace(".", ""))
+    
+    # Check if the book has a discount
     try:
-      precioOriginal = int(driver.find_elements(By.CSS_SELECTOR, '.infoProducto .precioTachado')[i].text.replace("$ ", "").replace(".", ""))
+      normalPrice = int(driver.find_elements(By.CSS_SELECTOR, '.infoProducto .precioTachado')[i].text.replace("$ ", "").replace(".", ""))
     except:
-      precioOriginal = precioAhora
+      normalPrice = priceNow
 
-    descuento = math.ceil(100-(100/(precioOriginal/precioAhora)))
+    descuento = math.ceil(100-(100/(normalPrice/priceNow)))
 
     libros = {
-      "titulo": titulo,
-      "precioAhora": precioAhora,
-      "precioOriginal": precioOriginal,
-      "descuento": descuento
+      "title": title,
+      "priceNow": priceNow,
+      "normalPrice": normalPrice,
+      "discount": descuento
     }
 
-    print(f"{libros["titulo"]}: ${libros['precioAhora']} ({libros["descuento"]}% OFF)")
+    print(f"{libros["title"]}: ${libros['priceNow']} ({libros["Discount"]}% OFF)")
 
 except Exception as e:
-  print("Ups. Hubo un problema: " + e)
+  print("Oops. There was an error: " + e)
   exit()
